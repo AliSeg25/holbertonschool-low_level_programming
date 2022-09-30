986189f6ddac64bce0d64f647ee8e7077b60d085
Ali <as.segueg2501@gmail.com>
2022-09-30 03:24:57 -0700

    55

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main
+ * main - Entrée point
  *
- * Return: Always 0.
+ * Return: Always 0 (Succes)
  */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("\"with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
