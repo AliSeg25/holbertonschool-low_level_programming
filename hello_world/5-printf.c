63c44356b43aebb27ae9f4c9db90860d0ea96f90
Ali <as.segueg2501@gmail.com>
2022-09-30 03:17:42 -0700

    5. Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main
  *
  * Return: Always 0.
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-
-  return (0);
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
