d4cafd4b12aa47b5c2f1efc53c76eda4f1397360
Ali <as.segueg2501@gmail.com>
2022-09-30 02:59:31 -0700

    5. Hello, printf

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,13 @@
 
 /**
     * main
-    *
+    *descrisption premier C
     * Return: Always 0.
     */
 
 int main(void)
+
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
