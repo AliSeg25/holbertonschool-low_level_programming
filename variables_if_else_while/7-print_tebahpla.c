dc72efab3a9087b47db93296f78ee78567d70039
Ali <as.segueg2501@gmail.com>
2022-10-03 09:37:41 -0700

    7-print_tebahpla.c

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -6,10 +6,16 @@
  */
 int main(void)
 {
-	char abc;
+/** de 0 a 9 */
+	int num = 0;
 
-	for (abc = 122; abc >= 97; abc--)
-		putchar(abc);
+	for (; num <= 9; num++)
+		putchar((num % 10) + '0');
+/** abc */
+	char l;
+
+	for (l = 97; l <= 102; l++)
+		putchar(l);
 	putchar('\n');
 	return (0);
 }
