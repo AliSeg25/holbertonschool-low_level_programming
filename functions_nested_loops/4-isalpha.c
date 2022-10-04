edf195e466a16cd92d897ad91dee12793d8016d3
Ali <as.segueg2501@gmail.com>
2022-10-04 03:59:14 -0700

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"main.h"
+#include "main.h"
+/**
+ * je cree un programme qui va faire la difference entre un caractere ou non
+ * si ces en caractere il return 1
+ * si non il return 0
+ */
+int _islower(int c)
+{
+	if ( c >= 97 && c <= 122 || c >= 65 && c <= 90 )
+		return (1);
+	else
+		return (0);
+}
