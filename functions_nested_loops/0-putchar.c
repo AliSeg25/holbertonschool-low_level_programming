0610ff77e41725955521e735f3955af2a818f60b
Ali <as.segueg2501@gmail.com>
2022-10-04 02:02:42 -0700

    1-alphabet.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 /**
  * main - return (0) premier exercicz putchar
- * return (0)
+ *
+ * return-(0)
  */
 int main(void)
 {
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - deuxieme exercicz
+ *
+ *amphbet
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+		printf ("%c", c);
+	printf ("\n");
+
+	return (0);
+}
