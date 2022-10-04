3cff4240476ae0142e27df2d64b551f367b11ea3
Ali <as.segueg2501@gmail.com>
2022-10-04 08:42:50 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * je cree un programme qui dira si le chiffre est posétif ou non
+ *
+ *
+ */
+int add(int nb1, int nb2)
+{
+	return (nb1 + nb2);
+}
