f79c4aacdf7c9267416dd8199e5d32e53ce007ae
Ali <as.segueg2501@gmail.com>
2022-10-04 05:03:29 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * je cree un programme qui dira si le chiffre est posétif ou non
+ *
+ *
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		i = (-1) * a;
+		return (a);
+	}
+	else if (a > 0)
+		return (a);
+	return (0);
+}
