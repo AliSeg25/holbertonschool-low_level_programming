991765e970beeb6f9a9f4a9c837d29a9a0a235ac
Ali <as.segueg2501@gmail.com>
2022-10-04 06:23:45 -0700

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+void jack_bauer(void)
+{
+	int heure;
+	int min;
+
+	for (heure = 0; heure < 24; heure++)
+	{
+		for (min = 0; min < 60; min ++)
+		{
+			_putchar((heure / 10) + '0');
+			_putchar((heure % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
