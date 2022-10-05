40c0532e74663d7b4f98e67319df719f4718466e
Ali <as.segueg2501@gmail.com>
2022-10-05 10:02:40 -0700

    6-abs.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,18 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * je cree un programme qui dira si le chiffre est posétif ou non
- *
- *
+ * _abs - Write a function that computes the absolute value of an integer.
+ * @a: ces la variable qui va nous servir
+ * Return: plusieur possibiliter
  */
 int _abs(int a)
 {
-	if (a < 0)
+	if (a > 0)
 	{
-		a = (-1) * a;
 		return (a);
 	}
-	else if (a > 0)
-		return (a);
+	else if (a < 0)
+	{
+		return (-a);
+
+	}
 	return (0);
 }
