02c3658856beb416890d142e8f04ccbd3222e0f0
Ali <as.segueg2501@gmail.com>
2022-10-05 10:55:39 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - Write a function that prints every minute of the day of Jack Ba
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
