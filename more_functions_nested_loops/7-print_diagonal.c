04c376e547aa23b5f4b5eec064a72e907cb600a3
Ali <as.segueg2501@gmail.com>
2022-10-06 04:46:43 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - 7. I feel like I am diagonally parked in a parallel universe
+ * @n: Variable
+ * Return: (\n);
+ */
+void print_diagonal(int n)
+{
+	int l= 0,
+		int m= 0;
+
+	if (n > 0)
+	{
+		for (; l < n; l++)
+		{
+			for (m = 0; m < l; m++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+
+	}
+	else
+		_putchar('\n');
+}
