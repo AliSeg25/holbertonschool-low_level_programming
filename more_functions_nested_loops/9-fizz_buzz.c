a9bda3db0e3c5bea3a133bd7589e88c360dae617
Ali <as.segueg2501@gmail.com>
2022-10-06 10:02:44 -0700

    9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - The “Fizz-Buzz test” is an view question designed to help filte
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 99; a++)
+	{
+		if (a % 3 == 0 && a % 5 != 0)
+			printf("Fizz ");
+		else if (a % 5 == 0 && a % 3 != 0)
+			printf("Buzz ");
+		else if (a % 15 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", a);
+	}
+	putchar('\n');
+	return (0);
+}
