c376f357f02232a4a2c366aa44242afedaa760ad
Ali <as.segueg2501@gmail.com>
2022-10-07 01:06:37 -0700

    8-print_base16

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -5,16 +5,18 @@
  *Return: Always (0) Success
  */
 int main(void)
-{
-/** variable */
-	int num = 0;
-	char l;
 
-	for (; num <= 9; num++)
-		putchar((num % 10) + '0');
-/** abc */
-	for (l = 97; l <= 102; l++)
-		putchar(l);
+{
+	char abc;
+	int nb = 0;
+
+	for (nb = '0'; nb <= '9'; nb++)
+		putchar(nb);
+	for (abc = 'a'; abc <= 'f'; abc++)
+		putchar(abc);
+
 	putchar('\n');
+
 	return (0);
+
 }
