eeacffc1e087132477b79e5ae2efb8e5d4f595be
Ali <as.segueg2501@gmail.com>
2022-10-07 01:21:55 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,21 +6,25 @@
  */
 int main(void)
 {
-/**déclaration variable */
-	int num = 0;
+	int nb = 0;
 
-/**début instruction */
-	for (; num <= 9; num++)
-{
-	putchar((num % 10) + '0');
-	if (num != 9)
-{
+	for (nb = '0'; nb <= '9'; nb++)
+	{
 
-/**espace virgule*/
-	putchar(',');
-	putchar(' ');
-}
-}
+
+		putchar(nb);
+
+		if (nb != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+
+
+
 	putchar('\n');
+
 	return (0);
 }
