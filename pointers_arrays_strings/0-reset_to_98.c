bc371833474212055c6684a537d9b0f1889143b2
Ali <as.segueg2501@gmail.com>
2022-10-10 00:20:46 -0700

    0-reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * reset_to_98 - Write a function that takes a pointer to an int as parameter a
+ * @n: ces la varaible
+ * Return: void
+ *
+ */
+void reset_to_98(int *n)
+{
+	int n = 98;
+	int *p = &n;
+
+}
