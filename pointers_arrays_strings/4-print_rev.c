2f7238ca30a1ca154c037450f3395d79b7acd3c6
Ali <as.segueg2501@gmail.com>
2022-10-10 09:01:47 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: variable a imprimer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int fin = 0;
+	int i = 0;
+
+	while (s != \0)
+	{
+		fin++;
+		s++;
+	}
+
+	for (i = fin; i >= 0; i--)
+	{
+		putchar(*s[i]);
+	}
+}
