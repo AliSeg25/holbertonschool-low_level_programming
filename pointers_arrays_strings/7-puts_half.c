473a26ff030965f4bbcae15b31ab0548a6c75927
Ali <as.segueg2501@gmail.com>
2022-10-10 11:46:32 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - 6. Half the lies they tell about me aren't true
+ * @str: input string
+ * Return:
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int nl = 0;
+
+	while (str[l] != '\O')
+	{
+		l++;
+	}
+
+	if ((l % 2) == 0)
+	{
+		nl = (l / 2);
+		while (str[nl] != '\0')
+			_putchar(*str);
+		nl++
+			}
+_putchar('\n')
+}
