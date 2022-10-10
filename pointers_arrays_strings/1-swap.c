a03dc9053b988dcf7c0ca1ab27053dd98ea60a48
Ali <as.segueg2501@gmail.com>
2022-10-10 01:26:34 -0700

    1-swap.c

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,12 +3,15 @@
  * swap_int - Write a function that swaps the values of two integers.
  * @a: valeur a changer
  * @b: valeur a changer
+ *
  * Return: void
  *
  */
 void swap_int(int *a, int *b);
 {
+	int echange = 0;
 
-	*a = 42;
-	*b = 98;
+	echange = *a;
+	*a = *b;
+	*b = echange;
 }
