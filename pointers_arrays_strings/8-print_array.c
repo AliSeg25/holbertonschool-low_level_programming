e03be07ed848f0cf881befd81245aa054041fd57
Ali <as.segueg2501@gmail.com>
2022-10-10 08:09:04 -0700

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - 8. Arrays are not pointers
+ * @a: variable tableau
+ * @n: nombre d'ellement tableaux
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+while (i < n)
+{
+printf("%d", a[i])
+if (i < (n - 1))
+printf(",")
+}
+	printf("/n")
+}
