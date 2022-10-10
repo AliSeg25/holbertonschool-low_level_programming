dc4c516f8c8f79dd99d2b71a2f65eb7b9c62ab93
Ali <as.segueg2501@gmail.com>
2022-10-10 10:01:23 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - 6. Half the lies they tell about me aren't true
+ *
+ * @str: contains char to be printed
+ * Return: void
+ */
+void puts2(char *str)
+{
+
+	while (str != '\0')
+	{
+		_putchar(str);
+		str += 2
+			}
+	_putchar('\n');
+}
