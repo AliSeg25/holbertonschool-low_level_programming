19969553d928c9d107f1cde357b1d41151a19d10
Ali <as.segueg2501@gmail.com>
2022-10-10 03:51:48 -0700

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - write a function that print a string
+ * @str: variable
+ * return: (0)
+ */
+void _puts(char *str)
+{
+char str[] = "I do not fear computers. I fear the lack of them ";
+
+	printf("%s", str);
+
+
+	return (0);
+}
