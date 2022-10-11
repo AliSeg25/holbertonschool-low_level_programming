9639a8db51639f619e92a03d0f1dbab682337870
Ali <as.segueg2501@gmail.com>
2022-10-11 02:16:16 -0700

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - 0. strcat
+ * @src: chaine premier
+ * @dest: chaine deuxieme
+ *
+ * Return: (dest)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int i = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[a] = src[i];
+		a++;
+		i++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
