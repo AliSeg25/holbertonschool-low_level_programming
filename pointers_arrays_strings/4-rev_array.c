efeac08fff532f67494f7f67794bd497c3d7433d
Ali <as.segueg2501@gmail.com>
2022-10-11 11:39:02 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * reverse_array - Write a function that reverses the contentvaleur
+ * @n: nombre d'ellemennt
+ * @a: tableau
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int a = 0;
+	int j = 0;
+
+	while (a[i] != '\0')
+
+	{
+		i++;
+	}
+
+	i--;
+
+	while (j < n)
+	{
+		a = a[i];
+		a[i] = a[j];
+		a[j] + a;
+		j++;
+		i--;
+	}
+
+}
