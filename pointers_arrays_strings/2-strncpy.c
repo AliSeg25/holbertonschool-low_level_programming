3f8ff150b57b9983914877795c7b475856997657
Ali <as.segueg2501@gmail.com>
2022-10-11 04:02:37 -0700

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @src: chaine a copie
+ * @dest: chaine coller
+ * @n: combien a copier
+ * Return: (dest)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0'
+
+		return (dest);
+}
