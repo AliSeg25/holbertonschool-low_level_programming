a51562b67cb633fc81999e3fb29b1bc41b0bf6e3
Ali <as.segueg2501@gmail.com>
2022-10-12 08:19:53 -0700

    3-strs

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - Write a function that gets the length of a prefix substring.
+ * @s: la chaine a rechercher
+ * @accept: la chaine qui recherche
+ * Return: (s) ou 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (*s == accept[j])
+				return (s);
+			j++;
+		}
+		s++;
+	}
+	return (0);
+}
