a3bb2838792c98c299757e87282e0259848982ea
Ali <as.segueg2501@gmail.com>
2022-10-12 06:54:56 -0700

    2-s

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,14 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s != '\0')
+	while (s[i] != '\0')
 	{
 		s++;
-		if (*s == c)
+		if (s[i] == c)
 		{
 			return (s);
 		}
-
+		i++
 	}
 	return (0);
 }
