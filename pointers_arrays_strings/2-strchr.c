05b687a5f439b1d4c2d6ec54355c286b974cf12b
Ali <as.segueg2501@gmail.com>
2022-10-12 03:48:43 -0700

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strchr - Write a function that locates a character in a string.
+ * @s: a trouve
+ * @c: a chercher
+ * Return: si trouve (s) si non (0)
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+
+		else
+		{
+			return (0);
+		}
+
+		i++;
+	}
+
+}
