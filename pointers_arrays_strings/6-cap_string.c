a3a217a003e7ac4500bcb9a83c41f924b3f187ed
Ali <as.segueg2501@gmail.com>
2022-10-13 02:32:22 -0700

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,18 +1,35 @@
-#include "main.h"
+#include"main.h"
 /**
- * cap_string - Write a function that capitalizes all words of a string.
- * @as: pointeur
- * Return: (âs)
+ * *cap_string - Write a function that capitalizes all words of a string.
+ * @st: pointeur
+ * Return: (st)
  */
-char *cap_string(char *as)
+
+char *cap_string(char *st)
 {
 	int i = 0;
 
-	while (as[i] != '\0')
+	if (st[0] >= 'a' && st[0] <= 'z')
 	{
-		if (as[i] >= 'a' && as[i] <= 'z')
-			as[i] -= 32;
+		st[0] -= 32;
+	}
+	while (st[i] != '\0')
+	{
+		if (st[i] == ',' || st[i] == '.' ||
+		    st[i] == ' ' || st[i] == ';' ||
+		    st[i] == '\n' || st[i] == '!' ||
+		    st[i] == '?' || st[i] == '"' ||
+		    st[i] == '(' || st[i] == ')' ||
+		    st[i] == '{' || st[i] == '}' ||
+		    st[i] == '\t')
+		{
+			if (st[i + 1] >= 'a' && st[i + 1] <= 'z')
+			{
+				st[i + 1] -= 32;
+			}
+		}
 		i++;
 	}
-	return (as);
+
+	return (st);
 }
