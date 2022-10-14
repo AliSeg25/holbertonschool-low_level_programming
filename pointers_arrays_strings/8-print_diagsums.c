230fbf14b527bf531978ca68e95d043be98b3260
Ali <as.segueg2501@gmail.com>
2022-10-14 06:14:31 -0700

    8-print

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,24 @@
-8-print_diagsums.c
+#include"main.h"
+/**
+ * print_diagsums - Write a function that prints the sum of the two diagona
+ * @a: pointer
+ * @tb: tableau longeur
+ *
+ * Return: void
+ */
+void print_diagsums(int *a, int tb)
+{
+	int i, j, p, l = 0, r = 0;
+
+	for (i = 0; i < tb; i++)
+	{
+		p = (i * tb) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < tb; j++)
+	{
+		p = (j * tb) + (tb - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
