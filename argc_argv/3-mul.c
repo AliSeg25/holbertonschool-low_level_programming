084862ced597077b307a040df637a2b1e1254118
Ali <as.segueg2501@gmail.com>
2022-10-18 03:58:02 -0700

    3-mul.c

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,16 +8,19 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j, r;
+	int i, j, r;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    i = atoi(argv[1]);
-    j = atoi(argv[2]);
-    r = i * j;
-    printf("%d\n", r);
-    return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+	r = i * j;
+
+	printf("%d\n", r);
+
+	return (0);
 }
