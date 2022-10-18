1707c54c1226feb02e137e2d11b12916c6b3df67
Ali <as.segueg2501@gmail.com>
2022-10-18 02:47:47 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the number of arguments passed into it.
+ * @argc: entier type int
+ * @argv: pointeur type char
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
