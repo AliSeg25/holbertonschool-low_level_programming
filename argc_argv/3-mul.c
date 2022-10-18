27d607b76883340050ba7114ba7c04c155dfb75f
Ali <as.segueg2501@gmail.com>
2022-10-18 03:53:32 -0700

    3-mul.c

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Write a program that multiplies two numbers.
+ * @argc: nombre argument
+ * @argv: pointeur
+ * Return: (0) ou (1)
+ */
+int main(int argc, char *argv[])
+{
+    int i, j, r;
+
+    if (argc != 3)
+    {
+        printf("Error");
+        return (1);
+    }
+    i = atoi(argv[1]);
+    j = atoi(argv[2]);
+    r = i * j;
+    printf("%d\n", r);
+    return (0);
+}
