0f17071e4913ce4040b8e284737f214d1fa545de
Ali <as.segueg2501@gmail.com>
2022-10-19 03:23:45 -0700

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Write a program that adds positive numbers.
+ * @argc: argument int
+ * @argv: pointeur sur tableau
+ * Return: (0) ou (1)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < argc, i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]))
+			{
+				printf("Error\n")
+					return(1)
+			}
+
+		}
+
+	}
+}
