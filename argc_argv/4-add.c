fecdb399778c366715ac5d46f34410c640cb0044
Ali <as.segueg2501@gmail.com>
2022-10-19 03:28:32 -0700

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for (i = 0; i < argc, i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
 		{
 			if (isdigit(argv[i][j]))
 			{
-				printf("Error\n")
-					return(1)
+				printf("Error\n");
+				return (1);
 			}
 
 		}
