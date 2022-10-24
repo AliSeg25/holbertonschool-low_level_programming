d9974cab087f20aa68c050f1e5029577bc217777
Ali <as.segueg2501@gmail.com>
2022-10-24 12:22:40 -0700

    2-calloc.c

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - Écrivez une fonction qui alloue de la mémoire pour un tableau, en utilisant malloc.
+ * @nmemb: le nombre d'element
+ * @size: la taille de l'ellement a allouer
+ * Return: (null) ou (ar)
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+
+	ar = malloc(size * nmemb);
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		free(ar[i]);
+
+	free(ar);
+
+	return (ar);
+}
