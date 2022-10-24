b5cc1da55cacc7fdf0d44941e00e04d2660852d9
Ali <as.segueg2501@gmail.com>
2022-10-24 10:08:28 -0700

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - concatene deux strings
+ * @s1: la premier partie a copier
+ * @s2: la seconde partie a copier
+ * @n: nombre a copier de S2
+ *
+ * Return: char pointer to newly allocated place in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *ar;
+    unsigned int i, j, r, k;
+
+    if(s1 == NULL)
+        s1 = 0;
+    else
+    {
+
+        for(i = 0; s1[i] != '\0'; i++)
+        ;
+    }
+
+    if(s2 == NULL)
+        s2 = 0;
+    else
+    {
+        for(j = 0; s2[j] != '\0'; j++)
+        ;
+    }
+
+    if (j > n)
+        j = n;
+
+    r = i + j + 1
+
+    ar = malloc(r * sizeof(char))
+
+    if (ar == NULL)
+        return(NULL);
+
+    for (k = 0; k < i; k++)
+        ar[k] = s1[k]
+
+    for (k = 0; k < j; k++)
+        ar[k + i] = s2[k]
+
+    ar[i + j] = '\0';
+
+    return (ar);
+}
