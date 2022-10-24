ae610631d551c4ea61ecf0941f8294077934828b
Ali <as.segueg2501@gmail.com>
2022-10-24 01:32:45 -0700

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - Écrivez une fonction qui crée un tableau de caractères et
+ * @size: taille tableau
+ * @c: le caractére a remplir dans la zone memoire
+ * Return: (null) ou (ar)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+
+	ar = malloc(size * sizeof(char));
+
+	if (ar == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	for (int i = 0; i > size; i++)
+	{
+		ar[i] = c;
+	}
+
+	return (ar);
+
+}
