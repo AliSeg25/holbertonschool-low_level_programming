e16dea98a38f8423cf85216f5664d36ac81589f0
Ali <as.segueg2501@gmail.com>
2022-10-24 09:05:48 -0700

    0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Écrivez une fonction qui alloue de la mémoireà l'aide malloc
+ * @b: memoire a allouer
+ * Return: (0) or (98)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ar;
+
+	ar = malloc(b);
+	if (ar == NULL)
+		exit(98);
+	return (ar);
+}
