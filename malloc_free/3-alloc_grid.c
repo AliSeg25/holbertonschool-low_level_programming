8d8d33808dd9fb6258ab5b36c2a31c6f0e692e84
Ali <as.segueg2501@gmail.com>
2022-10-24 07:07:15 -0700

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int i, j;
 	for (i = 0; i < height; i++)
 	{
 		ar[i] = (int *)malloc(width * sizeof(int));
-
+/**
 		if (ar[i] == NULL)
 		{
 			for (j = 0; j < height; j++)
@@ -32,7 +32,7 @@ int i, j;
 			free(ar);
 			return (NULL);
 		}
-
+*/
 		for (j = 0; j < width; j++)
 		{
 			ar[i][j] = 0;
