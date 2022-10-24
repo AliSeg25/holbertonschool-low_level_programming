4af7912d2b425e871c9b04c1a55f81f28fbe6fa3
Ali <as.segueg2501@gmail.com>
2022-10-24 13:29:01 -0700

    3-array_range.c

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,8 +21,10 @@ int *array_range(int min, int max)
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0, i < r, i++, min++)
+	for (i = 0, i < r, min++)
+	{
 		ar[i] = min;
-
+		i++;
+	}
 	return (ar);
 }
