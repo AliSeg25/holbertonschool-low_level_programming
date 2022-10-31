9b3831f7a4382b5aa5312a2d8f8288925a5bbe76
Your Name <as.segueg2501@gmail.com>
2022-10-31 11:01:58 -0700

    0-print_name.c

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+* print_name - Imprimer un nom
+* @name: string a imprime
+* @f: function pointe
+*/
+void print_name(char *name, void (*f)(char *))
+{
+void (*f)(*name);
+}
