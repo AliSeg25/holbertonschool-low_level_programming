02b74f5c1abd001d61af863e7f2c1723596f448a
Your Name <as.segueg2501@gmail.com>
2022-10-31 01:52:59 -0700

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -5,7 +5,7 @@
   * print_dog - Write a function that prints a struct dog
   * @d: imprimer
   */
-void print_dog(struct dog *d)
+/**void print_dog(struct dog *d)
 {
 	if (d == NULL)
 	{
@@ -24,6 +24,21 @@ void print_dog(struct dog *d)
 	{
 		printf("(nil)\n");
 	}
+	else
+		printf("Owner: %s\n", d->owner);
+}
+*/
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("\n");
+	if (d->name == NULL)
+		printf("(nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		printf("(nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
