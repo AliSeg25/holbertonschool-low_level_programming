952cb9a5bdce50b220272258c2257f117ea4f3eb
Your Name <as.segueg2501@gmail.com>
2022-10-31 04:17:13 -0700

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,19 +7,19 @@
   */
 void print_dog(struct dog *d)
 {
-if (d)
-{
-if (d->name)
-printf("Name: %s\n", d->name);
-else
-printf("Name: (nil)\n");
-printf("Age: %.6f\n", d->age);
-if (d->owner)
-printf("Owner: %s\n", d->owner);
-else
-printf("Owner: (nil)\n");
+	if (d)
+	{
+		if (d->name)
+				printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+			printf("Age: %.6f\n", d->age);
+		if (d->owner)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
 
-}
-else
-return;
+	}
+	else
+		return;
 }
