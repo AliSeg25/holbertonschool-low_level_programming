c008af56b9047ddb460b314b0b49836b68be18d6
Your Name <as.segueg2501@gmail.com>
2022-10-30 23:21:59 -0700

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog -  configuration des information du chien.
+ * @d: ces un poiteur sur le chien.
+ * @name: nom chien.
+ * @age: age chien.
+ * @owner: propriaitaire chien
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
