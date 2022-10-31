f4017ee79b604766a2a32fbb54f080aac74d9734
Your Name <as.segueg2501@gmail.com>
2022-10-30 23:08:10 -0700

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H_INCLUDED
+#define DOG_H_INCLUDED
+/**
+  * struct dog - présentation du chien
+  * @name : nom du chien
+  * @age : âge du chien
+  * @owner: proprietaire
+*/
+struct dog
+{
+char name[50];
+float age;
+char owner;
+};
+
+#endif /* DOG_H_INCLUDED*/
