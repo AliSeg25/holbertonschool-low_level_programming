a4bfc7e4a6b611fe20f59f71cfbe0003ca06460b
Your Name <as.segueg2501@gmail.com>
2022-11-01 05:03:29 -0700

    1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * sum_them_all - fonction qui imprime des nombres.
+ * @n: nombres arguments
+ * @separator: Imprimer ","
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+va_list arg;
+unsigned int i;
+
+va_start(arg, n);
+
+for (i = 0; i < n; i++;)
+{
+printf("%d", va_arg(arg, int));
+
+if (separator != NULL && i != (n - 1))
+printf("%s", separator);
+}
+printf("\n");
+
+va_end(arg);
+}
