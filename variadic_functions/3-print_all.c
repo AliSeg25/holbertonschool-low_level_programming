9ec814dd1c38c2b2f0f02eca57f7421a892906e4
Your Name <as.segueg2501@gmail.com>
2022-11-02 03:48:00 -0700

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,31 +10,36 @@ void print_all(const char * const format, ...)
 {
 	va_list arg;
 	va_start(arg, format);
+	int i = 0;
+	char *str;
 	char type; /*la variable contenir le type de format de [i]*/
 
-	while (format[i] != NUll)
+	while (format[i] != NULL && format[i] != '\0')
 	{
 		type = format[i];
 
 		switch (type)
 
 		{
-		case c:
+		case 'c':
 			printf("%c", va_arg(arg, int));
 
-		case i:
+		case 'i':
 			printf("%d", va_arg(arg, int));
 
-		case s:
-			str = va_arg(arg, c*)
-			printf("%s", str);
-
-		case f:
+		case 'f':
 			printf("%f", va_arg(arg, double));
 
+		case 's':
+			str = va_arg(arg, char*);
+			if (str == NULL)
+				str = "(nil)";
+
+			printf("%s", str);
 
 		}
 
+		if (type[i] == c || type[i] == i || type[i] == s|| type[i] == f)
 		i++;
 	}
 }
