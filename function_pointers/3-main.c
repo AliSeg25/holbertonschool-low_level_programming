600ef3504e1d1ec147f1873222bd59a3800b7ea7
Your Name <as.segueg2501@gmail.com>
2022-11-03 04:33:35 -0700

    3-m

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -22,12 +22,18 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	s = (argv[2]);
 
-	if (s == NULL && argv[2][1] != '\0')
+	if (s == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	res = get_op_func(s)(num1, num2);
 	printf("%d\n", res);
