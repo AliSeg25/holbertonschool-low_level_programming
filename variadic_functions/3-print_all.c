039bda286a30d9fdd6398c6a8529b7aa1a734167
Your Name <as.segueg2501@gmail.com>
2022-11-04 04:23:01 -0700

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,48 +2,71 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
+ * prin_int - imprimer un entier
+ * @arg: argument
+ * Return: ()
+*/
+void prin_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+
+/**
+ * prin_float - imprimer un float
+ * @arg: argument
+ * Return: ()
+*/
+void prin_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+
+/**
+ * prin_string - imprimer un string
+ * @arg: argument
+ * Return: ()
+ */
+void prin_string(va_list arg)
+{
+	printf("%s", va_arg(arg, char*));
+}
+
+/**
  * print_all - imprimer n'importe quoi.
  * @format: Le type
  * @...: nombre d 'argument
  */
 void print_all(const char * const format, ...)
 {
+	prin_t apf[] = {
+		{"c", prin_char},
+		{"i", prin_int},
+		{"f", prin_float},
+		{"s", prin_string},
+		{NULL, NULL}
+		};
+
 	va_list arg;
-	int i = 0;
-	char *str;
-	char type; /*la variable contenir le type de format de [i]*/
 
 	va_start(arg, format);
-	while (format != NULL && format[i] != '\0')
+
+	int i = 0;
+	int j = 0;
+
+	while (format && format[i] != '\0') /*format = ceis utilisateur*/
 	{
-		type = format[i];
-
-		switch (type)
+		while (apf[j].p)/*on parcoure le tableau de structure apf*/
 		{
-		case 'c':
-			printf("%c", va_arg(arg, int));
-			break;
-
-		case 'i':
-			printf("%d", va_arg(arg, int));
-			break;
-
-		case 'f':
-			printf("%f", va_arg(arg, double));
-			break;
-
-		case 's':
-			str = va_arg(arg, char*);
-			if (str == NULL)
-				str = "(nil)";
-
-			printf("%s", str);
-			break;
+			if (*apf[j].p == format[i])/*on compare la valeur*/
+			{
+				apf[j].f(arg);/*La on appel la bonne fonction*/
+				if (format[i + 1] == '\0')
+					break;
+				printf(", ");
+			}
+			j++;
 		}
-
-		if ((type == 'c' || type == 'i' || type == 's' || type == 'f')
-		    && format[i + 1])
-			printf(", ");
+		j = 0;
 		i++;
 	}
 	printf("\n");
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H_INCLUDED
 #define VARIADIC_FUNCTIONS_H_INCLUDED
 #include <stdarg.h>
+/**
+ * struct prin - structure pour 3-print_all
+ * @p : lire les valeur
+ * @f : fonction pointeur
+ */
+typedef struct prin
+{
+	char *p;
+	void (*f)(va_list arg);
+
+} prin_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
