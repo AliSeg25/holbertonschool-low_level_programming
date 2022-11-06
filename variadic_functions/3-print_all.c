78c0f49cb41d91befaf139ab588417fef642a052
Your Name <as.segueg2501@gmail.com>
2022-11-06 08:18:21 -0800

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -70,7 +70,7 @@ void print_all(const char * const format, ...)
 
 	va_start(arg, format);
 
-	while (format[i] != '\0') /*format = ceis utilisateur*/
+	while (format && format[i] != '\0') /*format = ceis utilisateur*/
 	{
 		while (apf[j].p)/*on parcoure le tableau de structure apf*/
 		{
