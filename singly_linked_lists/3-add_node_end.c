7778b26e59260656b1ce45b8e41ca32230b64b91
Your Name <as.segueg2501@gmail.com>
2022-11-14 07:51:47 -0800

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -31,8 +31,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	newli->len = l;
 	newli->next = *head;
 
-	if (head == NULL)
-	*head = newli;
+	if (*head == NULL)
+		*head = newli;
 
 	else
 	{
