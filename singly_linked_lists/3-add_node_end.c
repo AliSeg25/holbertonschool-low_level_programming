652da633290df9afec27a10752aeb8f7489d76ca
Your Name <as.segueg2501@gmail.com>
2022-11-14 06:16:54 -0800

    3-add_node_end.c

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -6,7 +6,7 @@
  * @str: The string
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node_end(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newli;
 	list_t *fin;
