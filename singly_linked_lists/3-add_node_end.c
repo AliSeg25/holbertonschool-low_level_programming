d2f04d1ad6168d5bb6a3939ba65f6659f3b448de
Your Name <as.segueg2501@gmail.com>
2022-11-14 06:15:37 -0800

    3-add_node_end.c

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - new node at the end
+ * @head: a pointer to the first node in the list
+ * @str: The string
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str);
+{
+	list_t *newli;
+	list_t *fin;
+	char *newstr;
+	int l = 0;
+
+	newli = malloc(sizeof(list_t));
+	if (newli == NULL)
+		return (NULL);
+
+	newstr = strdup(str);
+	if (newstr == NULL)
+	{
+		free(newli);
+		return (NULL);
+	}
+
+	while (str[l])
+	l++;
+
+	newli->str = newstr;
+	newli->len = l;
+	newli->next = *head;
+
+	if (*head ==  NULL)
+	*head = newli;
+
+	else
+	{
+		fin == *head;
+
+		while (fin->next != NULL)
+			fin = fin->next;
+	}
+	return (newli);
+}
