5057a33b87e7f78f79fb3ff4d477253cb2cf71f9
Your Name <as.segueg2501@gmail.com>
2022-11-14 08:06:25 -0800

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - Frees list.
+ * @head: a pointer
+ */
+void free_list(list_t *head)
+{
+	list_t temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
