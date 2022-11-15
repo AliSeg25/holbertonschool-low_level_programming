93cc617dc0589f9a2533481e2ba1135c44e40717
Your Name <as.segueg2501@gmail.com>
2022-11-15 01:30:18 -0800

    5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+65;6003;1c#include <stdlib.h>
 #include "lists.h"
 /**
  * free_listint2 - Frees list.
@@ -8,6 +8,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
+
+	if (head == NULL)
+		return(NULL);
 
 	while ((*head) != NULL)
 	{
