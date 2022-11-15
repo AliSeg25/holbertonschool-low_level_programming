795f3a5d8e05322e52f99846af0d93f71ff7679f
Your Name <as.segueg2501@gmail.com>
2022-11-15 05:12:07 -0800

    9-insert_nodeint.c

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	newli->n = n;
-	*temp = *head;
+	temp = *head;
 
 	for (node = 0; node < (idx - 1); node++)
 	{
