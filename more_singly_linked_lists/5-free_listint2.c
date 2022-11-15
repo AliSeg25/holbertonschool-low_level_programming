6c8e8e2355287b9c467873f873226ef4c4af894a
Your Name <as.segueg2501@gmail.com>
2022-11-15 01:27:32 -0800

    5-free_listint2.c

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	while ((*head) != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
 
-	head == NULL;
+	head = NULL;
 }
