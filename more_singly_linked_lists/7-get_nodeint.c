3f92b416e0791567d58267612246c72a7eeb8385
Your Name <as.segueg2501@gmail.com>
2022-11-15 02:19:37 -0800

    7-get_nodeint.c

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Write a function that deletes the head node
+ * of a listint_t linked list, and returns the head node’s data (n).
+ * @head: A pointer.
+ * @index: The index.
+ * Return:
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int node = 0;
+
+	for (node = 0; node < index; node++)
+	{
+		if (*head == NULL)
+			return (NULL);
+		
+		*head = (*head)->next;
+	}
+
+	return (*head);
+}
