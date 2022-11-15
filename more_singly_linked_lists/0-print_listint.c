cd749e1677a5d84dbe6680b5b1c828104615ea5f
Your Name <as.segueg2501@gmail.com>
2022-11-15 00:12:26 -0800

    0-print_listint.c

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer
+ */
+size_t print_listint(const listint_t *h)
+{
+	int l = 0;
+
+	while (h != '\0')
+	{
+		printf("%d\n", d->n);
+		h++;
+		d = d->next;
+	}
+
+	return (h);
+}
