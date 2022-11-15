611e0a900b72ab9761d17e521197668b86c45966
Your Name <as.segueg2501@gmail.com>
2022-11-15 02:20:46 -0800

    7-get_nodeint.c

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,11 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (node = 0; node < index; node++)
 	{
-		if (*head == NULL)
+		if (head == NULL)
 			return (NULL);
-		
-		*head = (*head)->next;
+
+		head = head->next;
 	}
 
-	return (*head);
+	return (head);
 }
