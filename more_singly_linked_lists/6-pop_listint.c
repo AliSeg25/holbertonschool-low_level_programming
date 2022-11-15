c0b06913256d765fd4db58dabb4d6abed7ebad4e
Your Name <as.segueg2501@gmail.com>
2022-11-15 02:06:12 -0800

    6-pop_listint.c

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * pop_listint - Frees list.
+ * pop_listint - Write a function that deletes the head node of a listint_t
+ * linked list, and returns the head node’s data (n)..
  * @head: a pointer
+ * Return: (NULL) ou (n)
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
 	if (head == NULL)
 		return (0);
 
 	temp = *head;
+	n = (*head)->n;
 	*head = (*head)->next;
 
 	free(temp);
 
-	return (*head);
+	return (n);
 }
