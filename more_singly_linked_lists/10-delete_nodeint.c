6c1f377e5b4bd954d75393647633a00cff467283
Your Name <as.segueg2501@gmail.com>
2022-11-15 07:34:37 -0800

    10-delete_nodeint.c

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	copy = *head;
 
-	for (node = 0; node < index; node++)
+	for (node = 0; node < (index - 1); node++)
 	{
 		copy = copy->next;
 	}
