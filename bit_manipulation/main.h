e05ede27b4b7eab178b6abd012829cabcf6136a6
Your Name <as.segueg2501@gmail.com>
2022-11-17 01:54:52 -0800

    0-binary_to_uint.c

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
