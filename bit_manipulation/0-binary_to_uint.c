797faf33c440a0e02ec999b423c4cee78ba0a909
Your Name <as.segueg2501@gmail.com>
2022-11-17 01:25:31 -0800

    0-binary_to_uint.c

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uint - Write a function that converts a binary number
+ * @b: A pointer
+ * Return: (NULL) ou (0) ou (1)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	int multi = 1;
+	int res = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	len--;
+
+	for (;len >= 0; len--)
+	{
+		res += (b[len] - '0') * multi;
+		multi *= 2;
+	}
+
+	return (res);
+}
