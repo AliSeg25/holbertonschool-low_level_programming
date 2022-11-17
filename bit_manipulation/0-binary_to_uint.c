a4a50938f3333b74af96a1295bec7caf4de6ad6d
Your Name <as.segueg2501@gmail.com>
2022-11-17 01:43:33 -0800

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (;len >= 0; len--)
 	{
-		if (b[len] =! '0' || b[len] =! '1')
+		if (b[len] != '0' || b[len] != '1')
 			return (0);
 
 		res += (b[len] - '0') * multi;
