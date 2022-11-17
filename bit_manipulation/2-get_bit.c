60257f41be0213d29419f5a83f7ce580bd13ab21
Your Name <as.segueg2501@gmail.com>
2022-11-17 04:06:42 -0800

    2-get_bit.ca

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -7,4 +7,12 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
- 
+	int res;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return(-1);
+	else
+		res = (n >> index) & 1;
+
+	return (res);
+}
