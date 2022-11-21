182944a8581a3a9e71cb57fddf2606bdc4d9da3b
Your Name <as.segueg2501@gmail.com>
2022-11-21 08:47:52 -0800

    5-get_dnodeint.c

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint -  Write a function that frees a dlistint_t list.
+ * @head: The pointeur
+ * @index: The index a return
+ * Return: The index ()
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+{
+	while (head < index)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+
+	return (head);
+}
