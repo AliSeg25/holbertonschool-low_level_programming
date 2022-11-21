968f73f08d1281d7c896d1ca989bb29b2652ed51
Your Name <as.segueg2501@gmail.com>
2022-11-21 09:59:47 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,17 +12,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 	dlistint_t *tmp = *h;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 
 	for (node = 0; node < (idx - 1); node++)
 	{
