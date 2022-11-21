0dabd87b87804503a067e8db5b3f6345d2f7c327
Your Name <as.segueg2501@gmail.com>
2022-11-21 04:41:35 -0800

    3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * close_file - Closes file descriptors.
+ * @file: The file closed.
+ * Return: ()
+ */
+vois close_file(char *file)
+{
+	int fi;
+
+	fi = close(file);
+
+	if (fi == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+/**
+ * alloc_memoire - Allocates to memoire.
+ * @file: The name file.
+ * Return: (buffer)
+ */
+char *alloc_memoire(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+
+		exit(99);
+	}
+	return (buffer);
+}
