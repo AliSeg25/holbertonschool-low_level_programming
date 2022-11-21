cf262a7eb1e1f53c50f4db72446fe553e3ed4a90
Your Name <as.segueg2501@gmail.com>
2022-11-21 02:44:04 -0800

    1-create_file.c

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_CREAT | O_RDWR| O_TRUNC, 0600);
-	w = write(STDOUT_FILENO, text_content, l);
+	w = write(o, text_content, l);
 
 	if (w == -1 || o == -1)
 		return (-1);
