423fab652b064dbddb78fdbe4c498d9d7b33e530
Your Name <as.segueg2501@gmail.com>
2022-11-21 08:38:13 -0800

    4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,10 +7,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
 
-	while (*head != NULL)
+	while (head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
