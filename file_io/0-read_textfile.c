0f4524407db321cf7c25185758667bef4477b713
Your Name <as.segueg2501@gmail.com>
2022-11-21 01:34:47 -0800

    0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * read_textfile - Write a function that reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the function should read and print.
+ *
+ * Return: (0) ou (w)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t *printl;
+	ssize_t o, r, w;
+
+	printl = malloc(sizeof(ssize_t));
+
+	if (printl == NULL)
+		return (0);
+
+	o = open(filename,  O_RDONLY);/*on ouvre le fichier en lecture*/
+	r = read(o, printl, letters);/*lire juqu'a count octets (letters)*/
+	w = write(1, printl, r);/*Ecrit jusqu'a tant de octet(r)dans lefichier*/
+
+
+	if (o == -1 || r == -1 || w != r)
+	{
+		free(printl);
+		return (0);
+	}
+
+	return (w);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -7,4 +11,4 @@ int append_text_to_file(const char *filename, char *text_content);
 
 
 
-#endif // MAIN_H_INCLUDED
+#endif /* MAIN_H_INCLUDED*/
