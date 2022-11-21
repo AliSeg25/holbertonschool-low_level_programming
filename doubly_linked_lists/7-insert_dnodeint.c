81175ec064f3b6e04c8c1ae2fab4a5b341c77b5a
Your Name <as.segueg2501@gmail.com>
2022-11-21 09:27:27 -0800

    7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index -  Write a function that frees a dlistint_t list.
+ * @h: The  double pointeur
+ * @idx: index add new node
+ * @n: values new node
+ * Return: (new)
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new;
+	dlistint_t *tmp;
+
+	tmp = *h;
+	new->n = n;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+
+	if (idx == NULL)
+	{
+		new->next = tmp;
+		tmp->next = new;
+
+		*head = new;
+	}
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (tmp == NULL)
+			return (NULL);
+		tmp = tmp->next;
+	}
+
+	new->next = tmp->next;
+	tmp->next = new;
+	new->prev = tmp;
+
+	tmp = tmp->next;
+
+	tmp->prev = new;
+
+	return (new);
+}
