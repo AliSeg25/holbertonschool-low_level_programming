8acb772d342d76185fd8efeccc5e4a4d5559fbac
Your Name <as.segueg2501@gmail.com>
2022-11-22 00:52:43 -0800

    hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,5 +1,8 @@
 #ifndef HASH_TABLES_H_INCLUDED
 #define HASH_TABLES_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -38,4 +41,4 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 
-#endif // HASH_TABLES_H_INCLUDED
+#endif /* HASH_TABLES_H_INCLUDED*/
