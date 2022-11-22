0258661d122774e7e14600c6f614d21cabc1ed20
Your Name <as.segueg2501@gmail.com>
2022-11-22 00:50:48 -0800

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - Write a function that creates a hash table.
+ * @size: The size of the array
+ * Return:(ht)
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+
+	ht->array = malloc(sizeof(hash_table_t) * size));
+	if (ht == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (NULL);
+}
