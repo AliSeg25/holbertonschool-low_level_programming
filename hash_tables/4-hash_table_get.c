f2867ac98db27e7d2800d3e1f9356abaab614ac4
Your Name <as.segueg2501@gmail.com>
2022-11-22 13:00:59 -0800

    4-hash_table_get.c

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Write a function that retrieves a value associe with key.
+ * @ht: is the hash table you want to look into
+ * @key: the keys of value
+ * Return: (NULL) ou (value)
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *) *key, ht->size);
+
+	if (index == NULL)
+		return (NULL);
+
+	node = ht->array[index];
+
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	if (node == NULL)
+		return (NULL);
+
+	else
+		return (node->value);
+}
