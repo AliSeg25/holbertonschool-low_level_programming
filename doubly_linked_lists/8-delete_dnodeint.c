cbbadba51e8eb6dab43871eb29ba250a17ed2070
Your Name <as.segueg2501@gmail.com>
2022-11-23 06:30:10 -0800

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		*head = (*head)->next;
+		*head = tmp->next;
 
 		if (*head != NULL)
 			(*head)->prev = NULL;
