210886b1b69abca6eb14895cf72b6870fa44cc4e
Your Name <as.segueg2501@gmail.com>
2022-11-23 05:49:50 -0800

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -32,10 +32,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 
 	new->n = n;
-	new->prev = tmp;
 	new->next = tmp->next;
 	tmp->next->prev = new;
 	tmp->next = new;
-
+	new->prev = tmp;
 	return (new);
 }
