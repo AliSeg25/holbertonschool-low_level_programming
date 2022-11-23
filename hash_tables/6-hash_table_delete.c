a8ae50633cc9e09e1e2c2be61f16ca39f6420689
Your Name <as.segueg2501@gmail.com>
2022-11-23 01:00:20 -0800

    6-hash_table_delete.c

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,7 +8,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_node_t *tmp;
-	hash_table_t *head = ht;
+	/*hash_table_t *head = ht;*/
 	hash_node_t *node;
 
 	for (i = 0; i < ht->size; i++)
@@ -27,6 +27,6 @@ void hash_table_delete(hash_table_t *ht)
 			}
 		}
 	}
-	free(head->array);
-	free(head);
+	free(ht->array);
+	free(ht);
 }
