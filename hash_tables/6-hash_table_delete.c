1b0f4c1be8afbd6973d3321c5c68cced48cc6bf3
Your Name <as.segueg2501@gmail.com>
2022-11-22 23:58:47 -0800

    6-hash_table_delete.c

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Write a function that deletes a hash table.
+ * @ht: is the hash table.
+ * Return:
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t temp;
+	hash_node_t node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+
+			while(node != NULL)
+			{
+				tmp = node->next;
+				free(node->value);
+				free(node->key);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+
+}
