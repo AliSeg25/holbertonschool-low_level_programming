34772a3aff8530db50c2e724d9a8606766e6b64a
Your Name <as.segueg2501@gmail.com>
2022-11-23 06:34:03 -0800

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -31,8 +31,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		tmp = tmp->next;
 	}
 
-	tmp->prev = tmp->next->prev;
 	tmp->prev->next = tmp->next;
+
+	if (tmp->next != NULL)
+		tmp->prev = tmp->next->prev;
 
 	return (1);
 }
