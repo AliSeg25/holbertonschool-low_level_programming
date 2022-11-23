a1b1d2987ad7db195596570a5e51cd7cfcf43699
Your Name <as.segueg2501@gmail.com>
2022-11-23 00:39:05 -0800

    5-hash_table_print.c

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Write a function that prints a hash table.
+ * @ht: is the hash table.
+ * Return:
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	printf("{")
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key node->value)
+					node = node->next;
+
+				if (node != NULL)
+					printf(", ");
+			}
+
+		}
+	}
+	printf("}");
+
+}
