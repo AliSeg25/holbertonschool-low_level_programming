36518f51937edac5234a9965f22b8b987ae94303
Your Name <as.segueg2501@gmail.com>
2022-11-23 00:41:09 -0800

    5-hash_table_print.c

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,7 +9,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	unsigned long int i;
 
-	printf("{")
+	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
