bfe0fe597013f54ef90447364ab1675a3788068f
Your Name <as.segueg2501@gmail.com>
2023-01-01 22:24:52 -0800

    0-linear.c

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+/**
+ * linear_search - Write a function that searches for a value in an array.
+ * @array: the array to search.
+ * @size: the size of the array.
+ * @value: the value.
+ * Return: the index or -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t idx = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (idx; idx < size; idx++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", idx, array[idx]);
+		if (array[idx] == value)
+			return (idx);
+	}
+
+	return (-1);
+}
