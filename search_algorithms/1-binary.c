0a1c3a85fbf99755beb05d26459873f4e2499ba2
Your Name <as.segueg2501@gmail.com>
2023-01-01 22:43:33 -0800

    1-binary.c

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Write a function that searches for a value in sortedarray.
+ * @array: the array to search.
+ * @size: the size of the array.
+ * @value: the value.
+ * Return: the index or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		size_t pivot = (left + right) / 2;
+
+		printf("Searching in array: ");
+
+		for (size_t i = left; i <= right; i++)
+			printf("%d, ", array[i]);
+
+		printf("\n");
+
+		printf("Value checked array[%ld] = [%d]\n", pivot, array[pivot]);
+
+		if (array[pivot] == value)
+			return (pivot);
+
+		else if (array[pivot] > value)
+			right = pivot - 1;
+		else
+			left = pivot + 1;
+	}
+
+	return (-1);
+}
